1dcabae78bd7429ff7bd85cdb4d1d4c2b6102033
Lefteris Kameris <lefteris.k@3elalliance.com>
2018-09-15 20:44:36 +0300

    Added Tests

--- webapp/app/BubbleType.php
+++ webapp/app/BubbleType.php
@@ -86,21 +86,21 @@ class BubbleType extends Model {
         BubbleType::query()->create([
             'name'       => 'Machine Learning',
             'slug'       => 'machine-learning',
-            'command'    => 'docker run --restart unless-stopped -p ##JUPYTER_PORT##:8888 -p ##NTERACT_PORT##:8889 -v ##WORKSPACE##:/opt/notebooks -d  --name ##NAME## threeel/jupyter',
+            'command'    => 'docker run --restart unless-stopped -p ##JUPYTER_PORT##:8888 --rm -p ##NTERACT_PORT##:8889 -v ##WORKSPACE##:/opt/notebooks -d  --name ##NAME## threeel/jupyter',
             'entrypoint' => '/opt/conda/bin/jupyter nteract --notebook-dir=/opt/notebooks --ip=\'*\' --port=8889 --no-browser --allow-root',
         ]);
 
         BubbleType::query()->create([
             'name'       => 'Natural Language Processing',
             'slug'       => 'natural-language-processing',
-            'command'    => 'docker run --restart unless-stopped -p ##JUPYTER_PORT##:8888 -p ##NTERACT_PORT##:8889 -v ##WORKSPACE##:/opt/notebooks -d  --name ##NAME## threeel/jupyter',
+            'command'    => 'docker run --restart unless-stopped -p ##JUPYTER_PORT##:8888 --rm -p ##NTERACT_PORT##:8889 -v ##WORKSPACE##:/opt/notebooks -d  --name ##NAME## threeel/jupyter',
             'entrypoint' => '',
         ]);
 
         BubbleType::query()->create([
             'name'       => 'Data Mining',
             'slug'       => 'data-mining',
-            'command'    => 'docker run --restart unless-stopped -p ##JUPYTER_PORT##:8888 -p ##NTERACT_PORT##:8889 -v ##WORKSPACE##:/opt/notebooks -d  --name ##NAME## threeel/jupyter',
+            'command'    => 'docker run --restart unless-stopped -p ##JUPYTER_PORT##:8888 --rm -p ##NTERACT_PORT##:8889 -v ##WORKSPACE##:/opt/notebooks -d  --name ##NAME## threeel/jupyter',
             'entrypoint' => '',
         ]);
 
