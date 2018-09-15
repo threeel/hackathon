792bde36acdb31af7262f93bd066793df695713d
Lefteris Kameris <lefteris.k@3elalliance.com>
2018-09-15 14:08:05 +0300

    bubbles

---
+++ webapp/app/Http/Controllers/BubbleTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\BubbleType;
+use Illuminate\Http\Request;
+
+class BubbleTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\BubbleType  $bubbleType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BubbleType $bubbleType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\BubbleType  $bubbleType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BubbleType $bubbleType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\BubbleType  $bubbleType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BubbleType $bubbleType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\BubbleType  $bubbleType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BubbleType $bubbleType)
+    {
+        //
+    }
+}
---
+++ webapp/database/factories/BubbleTypeFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\BubbleType::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
