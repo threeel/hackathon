eb5141c5e00816a2c1d4700bcc9cfb1d06e96d05
Lefteris Kameris <lefteris.k@3elalliance.com>
2018-09-15 12:43:27 +0300

    Added Argon Css + Landing Page with Sample data

--- webapp/webpack.mix.js
+++ webapp/webpack.mix.js
@@ -13,3 +13,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+
+mix.js('resources/js/argon.js','public/js');
+
+mix.browserSync({
+    proxy:"https://opendata.wip"
+});
