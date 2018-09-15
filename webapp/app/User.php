4914ed52be8eebaecf1f4f8bd32af1ec54865509
npafitis <npafit01@cs.ucy.ac.cy>
2018-09-15 22:46:33 +0300

    Electron desktop application

--- webapp/app/User.php
+++ webapp/app/User.php
@@ -31,6 +31,24 @@ class User extends Authenticatable {
     public function bubbles() {
 
         return $this->hasMany(DataBubble::class);
+        'name'       => 'Machine Learning',
+            'slug'       => 'machine-learning',
+            'command'    => 'docker run --restart unless-stopped -p ##JUPYTER_PORT##:8888 --rm -p ##NTERACT_PORT##:8889 -v ##WORKSPACE##:/opt/notebooks -d  --name ##NAME## threeel/jupyter',
+            'entrypoint' => '/opt/conda/bin/jupyter nteract --notebook-dir=/opt/notebooks --ip=\'*\' --port=8889 --no-browser --allow-root',
+        ]);
 
+        BubbleType::query()->create([
+            'name'       => 'Natural Language Processing',
+            'slug'       => 'natural-language-processing',
+            'command'    => 'docker run --restart unless-stopped -p ##JUPYTER_PORT##:8888 --rm -p ##NTERACT_PORT##:8889 -v ##WORKSPACE##:/opt/notebooks -d  --name ##NAME## threeel/jupyter',
+            'entrypoint' => '',
+        ]);
+
+        BubbleType::query()->create([
+            'name'       => 'Data Mining',
+            'slug'       => 'data-mining',
+            'command'    => 'docker run --restart unless-stopped -p ##JUPYTER_PORT##:8888 --rm -p ##NTERACT_PORT##:8889 -v ##WORKSPACE##:/opt/notebooks -d  --name ##NAME## threeel/jupyter',
+            'entrypoint' => '',
     }
+
 }
